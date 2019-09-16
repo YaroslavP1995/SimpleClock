679fdb4b8573a6954909088d7099e519edae2aef
Yaroslav <sk8enko@gmail.com>
2019-09-16 15:34:00 +0300

    test mapbox api

--- app/src/main/java/com/pysiak/simple/clock/ui/clock/ClockFragment.java
+++ app/src/main/java/com/pysiak/simple/clock/ui/clock/ClockFragment.java
@@ -1,5 +1,7 @@
 package com.pysiak.simple.clock.ui.clock;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -7,7 +9,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.pysiak.simple.clock.R;
+import com.mapbox.mapboxsdk.plugins.places.autocomplete.PlaceAutocomplete;
+import com.mapbox.mapboxsdk.plugins.places.autocomplete.model.PlaceOptions;
 
 /**
  * Fragment that implements {@link ClockView}
@@ -15,17 +22,32 @@ import com.pysiak.simple.clock.R;
  * @author Yaroslav P.
  */
 public class ClockFragment extends Fragment implements ClockView {
+    private static final int REQUEST_CODE_AUTOCOMPLETE = 1;
+    private MapboxMap mapboxMap;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.google_place, container, false);
-        return view;
+        return inflater.inflate(R.layout.clock_fragment, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        Mapbox.getInstance(getContext(), getString(R.string.map_box_key));
         ClockPresenter clockPresenter = new ClockPresenter();
         clockPresenter.attachView(this);
+        view.findViewById(R.id.fab_location_search).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new PlaceAutocomplete.IntentBuilder()
+                        .accessToken(Mapbox.getAccessToken() != null ? Mapbox.getAccessToken() : getString(R.string.map_box_key))
+                        .placeOptions(PlaceOptions.builder()
+                                .backgroundColor(Color.parseColor("#EEEEEE"))
+                                .limit(10)
+                                .build(PlaceOptions.MODE_CARDS))
+                        .build(getActivity());
+                startActivityForResult(intent, REQUEST_CODE_AUTOCOMPLETE);
+            }
+        });
     }
 }
